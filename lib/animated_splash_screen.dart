5aed0347a859568e684fc8c01a1263905bb59267
Yusuf Harby <youssef.srour789@gmail.com>
2025-11-08 05:39:22 +0200

    feat(splash-sceen): add animated spalsh screen

---
+++ lib/animated_splash_screen.dart
@@ -0,0 +1,49 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:tasky/app_colors.dart';
+
+class AnimatedSplashScreen extends StatefulWidget {
+  const AnimatedSplashScreen({super.key});
+
+  @override
+  State<AnimatedSplashScreen> createState() => _AnimatedSplashScreenState();
+}
+
+class _AnimatedSplashScreenState extends State<AnimatedSplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+        Duration(seconds: 4),
+        (){
+          Navigator.of(context).pushReplacementNamed('Onboarding Screen');
+        }
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.primary,
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 2,
+          children: [
+            FadeInLeft(
+              delay: Duration(milliseconds: 2000),
+              duration: Duration(milliseconds: 500),
+              child: Image.asset('assets/images/logo/Task_logo.png'),
+            ),
+            BounceInDown(
+              delay: Duration(milliseconds: 2500),
+              child: Transform.translate(
+                offset: const Offset(0, 10),
+                child: Image.asset('assets/images/logo/y_logo.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+abstract class AppColor {
+  static final Color primary = Color(0xff5F33E1);
+  static final Color title = Color(0xff24252C);
+  static final Color grayText = Color(0xff817D8D);
+  static final Color gray = Color(0xffAFAFAF);
+  static final Color white = Color(0xffffffff);
+}
