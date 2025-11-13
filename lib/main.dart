c9b1f81016986244599a27695ea50e7281fe9a53
Yusuf Harby <youssef.srour789@gmail.com>
2025-11-13 10:56:37 +0200

    - link project with firebase consol

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tasky/animated_splash_screen.dart';
+import 'package:tasky/firebase_options.dart';
 import 'package:tasky/home_screen.dart';
 import 'package:tasky/login_screen.dart';
 import 'package:tasky/onboarding_screen.dart';
 import 'package:tasky/register_screen.dart';
 
-void main (){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(Tasky());
 }
+
 class Tasky extends StatelessWidget {
   const Tasky({super.key});
 
@@ -17,11 +22,11 @@ class Tasky extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: AnimatedSplashScreen.pageRoute,
       routes: {
-        AnimatedSplashScreen.pageRoute : (_) => AnimatedSplashScreen(),
-        OnboardingScreen.pageRoute : (_) => OnboardingScreen(),
-        LoginScreen.pageRoute : (_) => LoginScreen(),
-        RegisterScreen.pageRoute : (_) => RegisterScreen(),
-        HomeScreen.pageRoute : (_) => HomeScreen(),
+        AnimatedSplashScreen.pageRoute: (_) => AnimatedSplashScreen(),
+        OnboardingScreen.pageRoute: (_) => OnboardingScreen(),
+        LoginScreen.pageRoute: (_) => LoginScreen(),
+        RegisterScreen.pageRoute: (_) => RegisterScreen(),
+        HomeScreen.pageRoute: (_) => HomeScreen(),
       },
     );
   }
