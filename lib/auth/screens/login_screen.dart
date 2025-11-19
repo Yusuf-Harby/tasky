9190f5420ca6884fc352974430459ce9f99e4ab1
Yusuf Harby <youssef.srour789@gmail.com>
2025-11-20 00:06:25 +0200

    - update

--- lib/auth/screens/login_screen.dart
+++ lib/auth/screens/login_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously, avoid_print, unused_local_variable
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tasky/custom_text_form_field_widget.dart';
